329e4dc40bc56f9081e94dec343a8408e8f5cecb
Unknown <hugo.leroux33@gmail.com>
2016-11-09 10:55:22 +0100

    Premier commit du projet

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    std::cout << "Quelle est ta couleur préférée ?" << std::endl;
+
+    int answer;
+    int question;
+
+    cin >> answer;
+    cout << answer << endl;
+    return 0;
+}
