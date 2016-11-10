5ad27d371e6cafc21869451a2ead47b05964f248
Unknown <hugo.leroux33@gmail.com>
2016-11-10 12:57:46 +0100

    Ajout de la fonction max
    Afin de récupérer la valeur max du tableau des réponses

--- main.cpp
+++ main.cpp
@@ -1,16 +1,36 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-int main() {
-    std::cout << "Quelle est ta couleur préférée ?" << std::endl;
+//Affichage du résultat
+int max(int reponses[], int length)
+{
+    int max = reponses[0];
 
+    for(int i = 1; i < 4; i++)
+    {
+        if(reponses[i] > max)
+            max = reponses[i];
+    }
+    return max;
+}
+
+int main() {
+    const int nb_reponses = 4;
+    int reponses[nb_reponses];
+
+    std::cout << "Quelle est ta couleur préférée ?" << std::endl;
 
     int answer;
     int question;
 
     cin >> answer;
     cout << answer << endl;
-    return 0;
+
+    //Récupération de la valeur max depuis la fonction max
+    max(reponses, 4);
+
 
 
 }
