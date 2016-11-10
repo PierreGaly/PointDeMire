c6e8dbf6d5e223d360ea90d06b9af6b83d07af2d
Unknown <hugo.leroux33@gmail.com>
2016-11-10 11:54:47 +0100

    Ajout questions

--- main.cpp
+++ main.cpp
@@ -4,10 +4,13 @@ using namespace std;
 int main() {
     std::cout << "Quelle est ta couleur préférée ?" << std::endl;
 
+
     int answer;
     int question;
 
     cin >> answer;
     cout << answer << endl;
     return 0;
+
+
 }
